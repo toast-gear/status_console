12c0b2fff4cbb64ea346171d61268617897cde09
Frank.Spencer <c.tait@outlook.com>
2017-01-16 22:34:32 +0000

    Removing navigation bar variable

--- website/lib-custom/js/metricsController.js
+++ website/lib-custom/js/metricsController.js
@@ -2,10 +2,10 @@
     angular.module('statusApp')
         .controller('metricsController', function ($rootScope, $location, localStorageService) {
             $rootScope.lastUpdateDateTime = '2016-10-21 13:00'
-            $rootScope.enableNavBar = true;
             if (localStorageService.isSupported !== true) {
                 console.log('YOUR BROWSER SUCKS GET A NEWER ONE, NEED TO IMPLEMENT SOME SORT OF PAGE / CONTROL DISABLE FOR THIS');
                 $rootScope.supportedBrowser = false;
+                $location.path('/')
             }
             else {
                 localStorageService.set('isAuthenticated', true);
