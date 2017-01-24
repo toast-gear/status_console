82626ee7bfbb6ea7ef636762393f3ca4d8705aac
Frank.Spencer <c.tait@outlook.com>
2017-01-24 21:16:40 +0000

    Small structure change

--- website/lib-custom/js/statusController.js
+++ website/lib-custom/js/statusController.js
@@ -2,12 +2,13 @@
     angular.module('statusApp')
         .controller('statusController', function ($rootScope, $location, $http, $log, localStorageService, metaService) {
             $log.debug('Status controller entered');
+            var baseUrl = ''
             if (localStorageService.sessionStart !== null) {
                 $log.debug('Setting local storage');
                 localStorageService.set('sessionStart', new Date());
             }
 
-            metaService.getLastStreamUpdateTime('')
+            metaService.getLastStreamUpdateTime(baseUrl + '/prod/api/meta/localservertime')
                 .then(function(response) {
                     $log.info('response date', response);
                     $rootScope.metricsLastUpdateDateTime = response.data;
