7491bc27518822b450bd79ebca55832affdfdcbc
Frank.Spencer <c.tait@outlook.com>
2017-03-08 19:57:52 +0000

    Further changes to support additional filtering

--- website/lib-custom/js/statusController.js
+++ website/lib-custom/js/statusController.js
@@ -1,12 +1,16 @@
 (function () {
     angular.module('statusApp')
-        .controller('statusController', function ($rootScope, $location, $http, $log, localStorageService, metaService, databaseService) {
+        .controller('statusController', function ($rootScope, $scope, $location, $http, $log, localStorageService, metaService, databaseService, localStoreService) {
             $log.info('Status controller entered');
             var baseUrl = '';
-            
-            if (localStorageService.sessionStart !== null) {
+                        
+            if (localStoreService.getLocalStorageKeyValue('sessionStart') === null) {
                 $log.info('Setting local storage session data');
                 localStorageService.set('sessionStart', new Date());
+            }
+            if (localStoreService.getLocalStorageKeyValue('numberOfStatusStreamDays') === null) {
+                $log.info('No status stream days found in session, setting to default');
+                localStorageService.set('numberOfStatusStreamDays', 30);
             }
 
             metaService.getLastStreamUpdateTime(baseUrl + '/api/meta/getlocalservertime')
@@ -19,7 +23,8 @@
                     $log.error(response);
                     $rootScope.metricsLastUpdateDateTime = 'ERROR';
                 });
-            databaseService.getStatusStreamMessages(baseUrl + '/api/database/getstatusstreammessages')
+            $log.info(localStoreService.getLocalStorageKeyValue('numberOfStatusStreamDays'));
+            databaseService.getStatusStreamMessages(baseUrl + '/api/database/getstatusstreammessages' + '?NumberOfDays=' + localStoreService.getLocalStorageKeyValue('numberOfStatusStreamDays'))
                 .then(function (response) {
                     $log.info('response date : ', response);
                     $rootScope.streamData = response.data;
@@ -28,5 +33,28 @@
                     $log.error('Exception on GET request');
                     $log.error(response);
                 });
+
+            $scope.loadMoreDays = function () {
+                $log.info('loadMoreDays event entered')
+                    var currentValue = localStoreService.getLocalStorageKeyValue('numberOfStatusStreamDays')
+                    localStorageService.set('numberOfStatusStreamDays', currentValue + 30);
+                    databaseService.getStatusStreamMessages(baseUrl + '/api/database/getstatusstreammessages' + '?NumberOfDays=' + localStoreService.getLocalStorageKeyValue('numberOfStatusStreamDays'))
+                                    .then(function (response) {
+                                        $log.info('response date : ', response);
+                                        $rootScope.streamData = response.data;
+                                    })
+            }
+            $scope.loadFewerDays = function () {
+                $log.info('loadFewerDays event entered')
+                if (localStoreService.getLocalStorageKeyValue('numberOfStatusStreamDays') > 0) {
+                    var currentValue = localStoreService.getLocalStorageKeyValue('numberOfStatusStreamDays')
+                    localStorageService.set('numberOfStatusStreamDays', currentValue - 30);
+                    databaseService.getStatusStreamMessages(baseUrl + '/api/database/getstatusstreammessages' + '?NumberOfDays=' + localStoreService.getLocalStorageKeyValue('numberOfStatusStreamDays'))
+                                    .then(function (response) {
+                                        $log.info('response date : ', response);
+                                        $rootScope.streamData = response.data;
+                                    })
+                }
+            }
         });
 })();
